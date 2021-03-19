f5984ce0e5295711838f3fec7ab1f03aef1bf71a
Richard Coleman <richardcoleman7@icloud.com>
2021-03-19 00:43:08 +0000

    Change offset for new BLTouch mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -991,7 +991,7 @@
  */
 // For Hero Me Gen5 + BLTouch + Medium Wing, Flat Mount
 // Z offset left at 0 for safety on first home
-#define NOZZLE_TO_PROBE_OFFSET { -64, -8, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -13, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
